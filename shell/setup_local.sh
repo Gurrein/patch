100189fd71187a70e600746a088debebec43122f
Kamal Maher <kmaher@kamal.mit.edu>
2019-12-16 19:32:20 -0500

    Edited message format

--- shell/setup_local.sh
+++ shell/setup_local.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # 1. Copy shell scripts to bin for better accessibility.
-echo "Need password to move patch shell scripts into your bin folder, which is a folder always in your path."
+echo
+echo "Need password to move some shell scripts into your bin folder, which is a folder always in your path."
 echo "This will allow access to each shell script without having to enter in the path. You need this convenience in your life."
 sudo cp ~/patch/shell/download_data.sh  /bin/
-sudp cp ~/patch/shell/upload_data.sh /bin/
-echo "Great. Now you can just type in 'upload_data.sh <date> <username>' without referencing the path to the script file. Good job."
-
+sudo cp ~/patch/shell/upload_data.sh /bin/
+echo
+echo "Great. Now you can just type in, for example, 'upload_data.sh <date> <username>' without referencing the path to the script file."
+echo "Good job."
+echo
