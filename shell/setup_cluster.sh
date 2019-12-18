71b410b1c35db93d0eb0ebd54c057a2fae10d0ee
Kamal Maher <kmaher@mit.edu>
2019-12-18 15:44:57 -0500

    Added .bash_profile required to specify path to new bin on cluster

--- shell/setup_cluster.sh
+++ shell/setup_cluster.sh
@@ -7,14 +7,17 @@ mkdir ~/bin
 for file in ~/patch/shell/*; do # Add all shell scripts to bin
 	cp $file ~/bin/
 done
+
+# 2. Add that bin file to path
+mv ~/patch/shell/.bash_profile ~/ # This file denotes the path to the bin that we will add to the user's default path
 source ~/.bash_profile # Activate changes
 
-# 2. Make folders for data and move all measureCam files there
+# 3. Make folders for data and move all measureCam files there
 mkdir /om2/user/$USER/data/
 mkdir /om2/user/$USER/data/patch_videos_cluster/
 for file in ~/patch/measure/*; do
 	cp $file /om2/user/$USER/data/patch_videos_cluster/
 done
 
-# 3. Make log folder to store analysis logs (i.e. the files containing MATLAB output for each tracking job).
+# 4. Make log folder to store analysis logs (i.e. the files containing MATLAB output for each tracking job).
 mkdir ~/log/
